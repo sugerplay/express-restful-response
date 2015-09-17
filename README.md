e30989921bf8f20aa4ce5f25aafe6eb448bfaa19
Yoojin Lee <leeyj255@naver.com>
2015-09-17 15:54:41 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # express-restful-response
-restful response for express
+Very simple way to response restfuly for express.
+
+## Usage
+
+```js
+var response = require("../modules/express-restful-response.js")
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    var output = {};
+    
+    ...
+    
+    response(res, output, {
+        status_code: 200,
+        status_msg: 'Success!'
+    })
+})
+```
+
+## Options
+
+```js
+response(res, output, {
+    status_code: 200,
+    status_msg: undefined,
+    jsonp: false
+})
+```
+
+## Status codes
+
+* 100: Continue
+* 101: Switching_Protocols
+
+* 200: OK
+* 201: Created
+* 202: Accepted
+* 203: Non_Authoritative_Information
+* 204: No_Content
+* 205: Reset_Content
+* 206: Partial_Content
+* 207: Multi_Status
+
+* 300: Multiple_Choices
+* 301: Moved_Permanently
+* 302: Found
+* 303: See_Other
+* 304: Not_Modified
+* 305: Use_Proxy
+* 306: Reserved
+* 307: Temporary_Redirect
+
+* 400: Bad_Request
+* 401: Unauthorized
+* 402: Payment_Required
+* 403: Forbidden
+* 404: Not_Found
+* 405: Method_Not_Allowed
+* 406: Not_Acceptable
+* 407: Proxy_Authentication
+* 408: Request_Timeout
+* 409: Conflict
+* 410: Gone
+* 411: Length_Required
+* 412: Precondition_Failed
+* 413: Request_Entity_Too_Large
+* 414: Request_URI_Too_Long
+* 415: Unsupported_Media_Type
+* 416: Requested_Range_Not_Satisfiable
+* 417: Expectation_Failed
+* 422: Unprocessable_Entity
+* 423: Locked
+* 424: Failed_Dependency
+
+* 500: Internal_Server_Error
+* 501: Not_Implemented
+* 502: Bad_Gateway
+* 503: Service_Unavailable
+* 504: Gateway_Timeout
+* 505: HTTP_Version_Not_Supported
+* 507: Insufficient_Storage
---
+++ index.js
@@ -0,0 +1,92 @@
+/**
+ * Usage
+ * res: response object
+ * output: output js object
+ * options: options for response
+ *      status_code: status code
+ *      status_msg: status message
+ */
+ 
+var setDefault = function (v, def) {
+    return (typeof v === 'undefined')? def : v;
+};
+    
+exports.response = function (res, output, options) {
+    options = setDefault(options, {});
+    options.status_code = setDefault(options.status_code, 200);
+    options.status_msg = setDefault(options.status_msg, undefined);
+    options.jsonp = setDefault(options.jsonp, false);
+    
+    // to do
+    options.encoding;
+    
+    if (typeof options.status_msg !== 'undefined') {
+        output.message = options.status_msg;
+    }
+    
+    res.setHeader("Content-Type", "text/json; charset=utf-8");
+    res.status(options.status_code);
+    
+    if (options.jsonp) {
+        res.jsonp(output);
+    }
+    else {
+        res.end(JSON.stringify(output));
+    }
+}
+
+// Informational
+exports.Continue = 100;
+exports.Switching_Protocols = 101;
+
+// Successful
+exports.OK = 200;
+exports.Created = 201;
+exports.Accepted = 202;
+exports.Non_Authoritative_Information = 203;
+exports.No_Content = 204;
+exports.Reset_Content = 205;
+exports.Partial_Content = 206;
+exports.Multi_Status = 207;
+
+// Redirection
+exports.Multiple_Choices = 300;
+exports.Moved_Permanently = 301;
+exports.Found = 302;
+exports.See_Other = 303;
+exports.Not_Modified = 304;
+exports.Use_Proxy = 305;
+exports.Reserved = 306;
+exports.Temporary_Redirect = 307;
+
+// Client Error
+exports.Bad_Request = 400;
+exports.Unauthorized = 401;
+exports.Payment_Required = 402;
+exports.Forbidden = 403;
+exports.Not_Found = 404;
+exports.Method_Not_Allowed = 405;
+exports.Not_Acceptable = 406;
+exports.Proxy_Authentication = 407;
+exports.Request_Timeout = 408;
+exports.Conflict = 409;
+exports.Gone = 410;
+exports.Length_Required = 411;
+exports.Precondition_Failed = 412;
+exports.Request_Entity_Too_Large = 413;
+exports.Request_URI_Too_Long = 414;
+exports.Unsupported_Media_Type = 415;
+exports.Requested_Range_Not_Satisfiable = 416;
+exports.Expectation_Failed = 417;
+exports.Unprocessable_Entity = 422;
+exports.Locked = 423;
+exports.Failed_Dependency = 424;
+
+// Server Error
+exports.Internal_Server_Error = 500;
+exports.Not_Implemented = 501;
+exports.Bad_Gateway = 502;
+exports.Service_Unavailable = 503;
+exports.Gateway_Timeout = 504;
+exports.HTTP_Version_Not_Supported = 505;
+exports.Insufficient_Storage = 507;
